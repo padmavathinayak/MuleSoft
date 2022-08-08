2dbc1b3ae38a2118f437c37293e7922def73b47a
padmavathinayak <85066858+padmavathinayak@users.noreply.github.com>
2022-08-08 23:43:55 +0530

    Create getData.java

---
+++ getData.java
@@ -0,0 +1,38 @@
+package Misc;
+
+import java.sql.*;
+import java.util.*;
+
+public class GetData {
+	
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","Padma");
+			
+			Statement st = con.createStatement();
+			
+			st.executeUpdate("USE Movies;");
+	
+			
+			ResultSet res = st.executeQuery("SELECT * FROM favorites WHERE main_ = \"Hrithik\" ");
+			
+			System.out.println("NAME \t\t MAIN ACTOR \t\t RELEASE YEAR \t\t DIRECTOR\n");
+			
+			while(res.next()) {
+				
+				System.out.println(res.getString(1) + " \t\t " + res.getString(2) + " \t\t " + res.getInt(3) + " \t\t " + res.getString(4));
+				
+			}
+						
+			System.out.println("Complete");
+			
+			con.close();
+			
+		}
+		catch(Exception e) {
+			System.out.print(e);
+		}
+	}
+
+}
